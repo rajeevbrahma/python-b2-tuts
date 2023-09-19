3b79bfb04fda4cd9ab1fa6647a43ee436d563749
rajeevbrahma <rajeevbrahma@hotmail.com>
2023-09-19 19:19:27 +0530

    day 9 & 10 lessons

---
+++ day-10-09182023/tuple_intro.py
@@ -0,0 +1,24 @@
+"""
+Tuples are ordered
+    To store multiple values of various datatypes
+    Can access using the index
+    "immutable / unchangeable"
+
+    (1, 2, 3)
+     0, 1, 2
+    -3 -2 -1
+
+"""
+
+# tuple initialisation
+tuple_variable = (1,2,3)
+tuple_variable_without_braces = 1,2,3
+tuple_variable_with_comma = (1,)
+
+
+# Define a tuple with Month names
+# Define a tuple with squares of 2
+# Define a tuple with binary number 8,16,32 (every number sperated with ',')
+
+# Write a simple code to extract every 2 and 3 element from the above tuples.
+
