815c118efc23213f408b867662ca3ce5894a19d3
rajeevbrahma <rajeevbrahma@hotmail.com>
2023-09-21 06:10:39 +0530

    day 11 lesson

---
+++ day-11-09192023/example.py
@@ -0,0 +1,12 @@
+his_age = 10
+his_qulication = 2
+"""
+    age > 18
+minimum_qualification >= 2
+give him admission to this university 
+
+"""
+if (his_age > 18 or his_qulication >=2):
+    print ("Eligible for admission")
+else:
+    print ("Not Eligible")
---
+++ day-11-09192023/operators.py
@@ -0,0 +1,48 @@
+assignee = 10
+assignee += 10   # => assignee = assignee + 10
+print (assignee)
+assignee -= 10   # => assignee = assignee - 10
+print (assignee)
+assignee *= 10   # => assignee = assignee * 10
+print (assignee)
+assignee **= 10   # => assignee = assignee ** 10
+print (assignee)
+assignee /= 10   # => assignee = assignee / 10
+print (assignee)
+assignee %= 10   # => assignee = assignee % 10
+print (assignee)
+assignee //= 10   # => assignee = assignee // 10
+print (assignee)
+
+# comparison
+print (1==2)
+print (1!=2)
+print (1>2)
+print (1<=2)
+print (1>=2)
+
+# Logical Operators
+print (1 and 2)
+print (1 or 2)
+print (not 1)
+
+"""
+    Binary numbers 
+    
+    2^3 2^2 2^1 2^0 
+    0   0     0   0   - 1
+    0   0     1   0   - 2 
+    0   0     1   1   - 3
+    0   1     0   0   - 4
+    0   1     0   1   - 5
+
+left shift of a number 
+    4 << 1  = 8
+
+
+right shift of a number
+    4 >> 1  = 2
+
+
+"""
+
