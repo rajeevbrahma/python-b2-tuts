dffe51373b57564102d3f35a549d528c43130d96
rajeevbrahma <rajeevbrahma@hotmail.com>
2023-09-21 06:10:03 +0530

    built in funcs

---
+++ built-in-funcs.py
@@ -0,0 +1,4 @@
+print () # Displays in the standard output console
+id()   # Return the memory location of the literal / variable given
+type() # Return the data type of the literal / variable given
+bin()  # Return the binary representation of an integer.
