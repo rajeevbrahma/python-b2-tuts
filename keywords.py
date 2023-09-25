9a91456a5e412d99dde54a7fd971494c9f5ca67c
rajeevbrahma <rajeevbrahma@hotmail.com>
2023-09-25 21:29:55 +0530

    keywords

---
+++ keywords.py
@@ -0,0 +1,23 @@
+"""
+
+Keywords:
+    Python Keywords are some predefined and reserved words 
+    in Python that have special meanings.
+
+----------------------------------------------------------------------
+Type            |      Keywords             |        Description
+----------------------------------------------------------------------
+Value           | True, False, None         | These has some value
+Operator        | and, or, not, in, is      | These are used to perform
+                |                           | logical operations  
+Control flow    | if, elif, else            | These used to make 
+                |                           | conditional statements
+                |                           |
+                |                           |
+                |                           |
+                |                           |
+                |                           |
+
+
+"""
+
