fe171929c0a363c1da9ccbdba7d3a1ff178094f5
rajeevbrahma <rajeevbrahma@hotmail.com>
2023-10-11 08:07:18 +0530

    keywords

--- keywords.py
+++ keywords.py
@@ -12,7 +12,7 @@ Operator        | and, or, not, in, is      | These are used to perform
                 |                           | logical operations  
 Control flow    | if, elif, else            | These used to make 
                 |                           | conditional statements
-iterative       | for,while                 |
+iterative       | for,while                 | These are used for iterative statements
                 |                           |
                 |                           |
                 |                           |
@@ -20,6 +20,17 @@ iterative       | for,while                 |
                                               empty functions / empty classes
                 |  def                      | We can use it to define a function
 
-                | global
+Variable        | global                    | we can use to define a variable as globale variable
+Handling        | try, except, finally      | we can use to handle exceptions
+                | import                    | we can use to import modules 
+                | break                     | we can use to break a loop
+                | continue                  | we can use to continue a loop by skipping the current instance
+                | from                      | we can use to import specific functions from a module
+                | as                        | we can use to rename a module / function / class
+                |                           |
+
+
+      
+
 """ 
 
